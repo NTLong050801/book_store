ad287a252841b28797c34dbc0f6e7d13c5bc788c
minh7721 <nhatminh7721@gmail.com>
2021-11-18 23:47:39 +0700

    o

--- QuanLy/index.php
+++ QuanLy/index.php
@@ -3,6 +3,16 @@ include('./header_footer/header.php');
 ?>
 <div class="main-content">
     <div class="container-fluid">
+        <form class="row g-3 mt-3 d-flex justify-content-center">
+         
+            <div class="col-auto">
+                <label for="inputPassword2" class="visually-hidden">Nhập tên sách...</label>
+                <input type="text" class="form-control" id="inputPassword2" placeholder="Nhập tên sách...">
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn mb-3"><i class="fas fa-search"></i></button>
+            </div>
+        </form>
         <a href="./them.php"><button class="btn btn-success mt-3">Thêm sách</button></a>
         <?php
         if (isset($_SESSION['delOK'])) {
@@ -21,13 +31,6 @@ include('./header_footer/header.php');
                     <th scope="col">Tên</th>
                     <th scope="col">Tác giả</th>
                     <th scope="col">Nhà XB</th>
-                    <th scope="col">Thể loại</th>
-                    <th scope="col">Năm xuất bản</th>
-                    <th scope="col">Số trang</th>
-                    <th scope="col">Số lượng</th>
-                    <th scope="col">Mô tả</th>
-                    <th scope="col">Ngôn ngữ</th>
-                    <th scope="col">Lượt mua</th>
                     <th scope="col">Ảnh</th>
                     <th scope="col">Giá</th>
                     <th scope="col">Giảm giá</th>
@@ -48,14 +51,7 @@ include('./header_footer/header.php');
                             <td><?php echo $row['s_ten'] ?></td>
                             <td><?php echo $row['tg_ten'] ?></td>
                             <td><?php echo $row['nxb'] ?></td>
-                            <td><?php echo $row['tl_ten'] ?></td>
-                            <td><?php echo $row['namxuatban'] ?></td>
-                            <td><?php echo $row['sotrang'] ?></td>
-                            <td><?php echo $row['soluong'] ?></td>
-                            <td><?php echo $row['mota'] ?></td>
-                            <td><?php echo $row['ngonngu'] ?></td>
-                            <td><?php echo $row['luotmua'] ?></td>
-                            <td><img src="../img/<?php echo $row['anh']?>" alt="" class="img-fluid"></td>
+                            <td><img src="../img/<?php echo $row['anh'] ?>" alt="" style="width: 200px;" class="img-fluid"></td>
                             <td><?php echo $row['s_gia'] ?></td>
                             <td><?php echo $row['s_giamgia'] ?></td>
                             <td>
