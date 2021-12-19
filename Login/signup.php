85e8e4b056ac7113642b8537948eda886637435b
NTLong050801 <90180848+NTLong050801@users.noreply.github.com>
2021-12-19 19:55:12 +0700

    update

--- Login/signup.php
+++ Login/signup.php
@@ -11,8 +11,25 @@ use PHPMailer\PHPMailer\Exception;
 require './Email/Exception.php';
 require './Email/PHPMailer.php';
 require './Email/SMTP.php';
-
 ?>
+<!-- <style>
+    .login {
+        width: 100%;
+    }
+
+    .img-fluid {
+        border-radius: 20px;
+    }
+
+    .check_input {
+        color: red;
+    }
+
+    .err {
+        color: red;
+        font-size: 12px;
+    }
+</style> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,24 +41,7 @@ require './Email/SMTP.php';
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <style>
-        .login {
-            width: 100%;
-        }
 
-        .img-fluid {
-            border-radius: 20px;
-        }
-
-        .check_input {
-            color: red;
-        }
-
-        .err {
-            color: red;
-            font-size: 12px;
-        }
-    </style>
 </head>
 
 <body>
@@ -54,69 +54,6 @@ require './Email/SMTP.php';
                 <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
                     <h2>Đăng ký</h2>
                     <br>
-                    <?php
-                    if (isset($_POST['btn_dk'])) {
-                        $k_ten = $_POST['k_ten'];
-                        setcookie("coo_k_ten", $k_ten, time() + 60 * 5);
-                        $k_gioitinh = $_POST['k_gioitinh'];
-                        $k_ngsinh = $_POST['k_ngsinh'];
-                        setcookie("coo_k_ngsinh", $k_ngsinh, time() + 60 * 5);
-                        $k_email = $_POST['k_email'];
-                        $k_pass = $_POST['k_pass'];
-                        $k_pass_again = $_POST['k_pass_again'];
-                        $k_sdt = $_POST['k_sdt'];
-                        $k_diachi = $_POST['k_diachi'];
-                        $qr = slt_email($k_email);
-                        if (mysqli_num_rows($qr) > 0) {
-                        } else {
-                            if ($k_pass == $k_pass_again) {
-                                setcookie("code", mt_rand(1000, 9999), time() + 300);
-                                if (isset($_COOKIE['code'])) {
-                                    $code = $_COOKIE['code'];
-                                    $mail = new PHPMailer(true);
-                                    try {
-                                        //Server settings
-                                        $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output
-                                        $mail->isSMTP(); // gửi mail SMTP
-                                        $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
-                                        $mail->SMTPAuth = true; // Enable SMTP authentication
-                                        $mail->Username = 'loglog050801@gmail.com'; // SMTP username
-                                        $mail->Password = 'kmpzyvxwqrgezkgw'; // SMTP password
-                                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-                                        $mail->Port = 587; // TCP port to connect to
-                                        $mail->CharSet = 'UTF-8';
-                                        //Recipients
-                                        $mail->setFrom('loglog050801@gmail.com', 'Mã xác nhận đăng ký tài khoả đăng ký tài khoản');
-
-                                        $mail->addReplyTo('loglog050801@gmail.com', 'Mã xác nhận đăng ký tài khoản');
-
-                                        $mail->addAddress($k_email); // Add a recipient
-                                        // Content
-                                        $mail->isHTML(true);   // Set email format to HTML
-                                        $tieude = '[Mã xác nhận] Lấy lại mật khẩu - Trường DH Thủy Lợi';
-                                        $mail->Subject = $tieude;
-                                        // Mail body content 
-                                        $bodyContent = '<p>Thân gửi người dùng . </h1>';
-                                        $bodyContent .= '<p>Vui lòng nhấn vào linh sau để kích hoạt tài khoản :"</p>';
-                                        $bodyContent .= '<p>Nhấn vào đây để kích hoạt <a href="http://localhost/finsh/book_store/Login/process_dk.php?email=' . $k_email . '&code=' . $code . '">Xác nhận</a></p>';
-                                        $bodyContent .= '<p>Note: Mã xác nhận tồn tại trong 5 phút .</p>';
-                                        $bodyContent .= '<p>Chú ý: Không trả lời thư này.Xin trân trọng cảm ơn !</p>';
-                                        $bodyContent .= '<p><b>Thân mến!</b></p>';
-                                        $mail->Body = $bodyContent;
-                                        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-                                        if ($mail->send()) {
-                                            echo 'Thư đã được gửi đi';
-                                        } else {
-                                            echo 'Lỗi. Thư chưa gửi được';
-                                        }
-                                    } catch (Exception $e) {
-                                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    ?>
                     <form action="" method="POST">
                         <div class="mb-3 row">
                             <label for="staticEmail" class="col-sm-3 col-form-label">Họ Tên</label>
@@ -201,9 +138,72 @@ require './Email/SMTP.php';
         </div>
         </div>
     </section>
-    
+    <?php
+                    if (isset($_POST['btn_dk'])) {
+                        $k_ten = $_POST['k_ten'];
+                        setcookie("coo_k_ten", $k_ten, time() + 60 * 5);
+                        $k_gioitinh = $_POST['k_gioitinh'];
+                        $k_ngsinh = $_POST['k_ngsinh'];
+                        setcookie("coo_k_ngsinh", $k_ngsinh, time() + 60 * 5);
+                        $k_email = $_POST['k_email'];
+                        $k_pass = $_POST['k_pass'];
+                        $k_pass_again = $_POST['k_pass_again'];
+                        $k_sdt = $_POST['k_sdt'];
+                        $k_diachi = $_POST['k_diachi'];
+                        $qr = slt_email($k_email);
+                        if (mysqli_num_rows($qr) > 0) {
+                        } else {
+                            if ($k_pass == $k_pass_again) {
+                                setcookie("code", mt_rand(1000, 9999), time() + 300);
+                                if (isset($_COOKIE['code'])) {
+                                    $code = $_COOKIE['code'];
+                                    $code_k = md5($code);
+                                    $mail = new PHPMailer(true);
+                                    try {
+                                        //Server settings
+                                        $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output
+                                        $mail->isSMTP(); // gửi mail SMTP
+                                        $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+                                        $mail->SMTPAuth = true; // Enable SMTP authentication
+                                        $mail->Username = 'loglog050801@gmail.com'; // SMTP username
+                                        $mail->Password = 'kmpzyvxwqrgezkgw'; // SMTP password
+                                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+                                        $mail->Port = 587; // TCP port to connect to
+                                        $mail->CharSet = 'UTF-8';
+                                        //Recipients
+                                        $mail->setFrom('loglog050801@gmail.com', 'Mã xác nhận đăng ký tài khoả đăng ký tài khoản');
 
+                                        $mail->addReplyTo('loglog050801@gmail.com', 'Mã xác nhận đăng ký tài khoản');
 
+                                        $mail->addAddress($k_email); // Add a recipient
+                                        // Content
+                                        $mail->isHTML(true);   // Set email format to HTML
+                                        $tieude = '[Mã xác nhận] Lấy lại mật khẩu - Trường DH Thủy Lợi';
+                                        $mail->Subject = $tieude;
+                                        // Mail body content 
+                                        $bodyContent = '<p>Thân gửi người dùng . </h1>';
+                                        $bodyContent .= '<p>Vui lòng nhấn vào linh sau để kích hoạt tài khoản :"</p>';
+                                        $bodyContent .= '<p>Nhấn vào đây để kích hoạt <a href="http://localhost:88/finsh/book_store/Login/process_dk.php?email=' . $k_email . '&code=' . $code_k . '">Xác nhận</a></p>';
+                                        $bodyContent .= '<p>Note: Mã xác nhận tồn tại trong 5 phút .</p>';
+                                        $bodyContent .= '<p>Chú ý: Không trả lời thư này.Xin trân trọng cảm ơn !</p>';
+                                        $bodyContent .= '<p><b>Thân mến!</b></p>';
+                                        $mail->Body = $bodyContent;
+                                        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                                        if ($mail->send()) {
+                                            echo 'Thư đã được gửi đi';
+                                            
+                                        } else {
+                                            echo 'Lỗi. Thư chưa gửi được';
+                                        }
+                                    } catch (Exception $e) {
+                                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                                    }
+                                }
+                             
+                            }
+                        }
+                    }
+                    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
